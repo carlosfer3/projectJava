11c2ee248f0bf8f425bec90b82abbf48cdafa143
Carlos <carlos.fernandez.i@uni.pe>
2025-04-01 18:07:50 -0500

    primer commit

---
+++ ChessProyec/src/chessproyec/ChessProyec.java
@@ -0,0 +1,9 @@
+package chessproyec;
+
+public class chessproyec{
+    public static void main(String[] args) {
+        King rey = new King();
+        rey.Representation(); // Debería imprimir el dibujo del Rey
+    }
+}
+
---
+++ ChessProyec/src/chessproyec/Piece.java
@@ -0,0 +1,9 @@
+package chessproyec;
+
+public class Piece {
+    protected String shape;
+    public void Representation()
+    {
+        System.out.println(shape);
+    }
+}
