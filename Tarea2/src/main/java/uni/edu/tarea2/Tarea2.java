c7ec5d0273c4544b73b3228c8f01f31cb5fa352e
Carlos <carlos.fernandez.i@uni.pe>
2025-04-02 20:04:56 -0500

    Calculadora con interfaces

---
+++ Tarea2/src/main/java/uni/edu/tarea2/Tarea2.java
@@ -0,0 +1,10 @@
+package uni.edu.tarea2;
+
+public class Tarea2 {
+
+    public static void main(String[] args) {
+        Calculadora principal = new Calculadora();
+        principal.setVisible(true);
+        principal.setLocationRelativeTo(null);
+    }
+}
